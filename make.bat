b80328e4394b59e6050089515cd0c1cd4d994fd4
clwt <pink.devel@proton.me>
2025-02-02 09:58:38 +0000

    init: add basic JNI bindings

--- make.bat
+++ make.bat
@@ -4,4 +4,4 @@ call pink dll
 
 windres -i serenity.rc -o serenity.o
 
-gcc -Os -o serenity.dll -s -shared out_dll.c dll\stub.c serenity.o -Wl,--subsystem,windows
+gcc -Os -o serenity.dll -s jni\jvm.lib -shared out_dll.c dll\stub.c serenity.o -Wl,--subsystem,windows
